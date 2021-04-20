d1475ef0c4c2d777f33c557c20d73c8d60b59ec6
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-20 14:26:39 +0200

    case of a list of str on BL

--- notch_filter.py
+++ notch_filter.py
@@ -369,7 +369,12 @@ def main():
     picks = config['param_picks_by_channel_indices']
 
     # In case of a list of str
-    # if isinstance(picks, str) and picks.find("[") != -1 and picks is not None:
+    if isinstance(picks, str) and picks.find("[") != -1 and picks is not None:
+        picks = picks.replace('[', '')
+        picks = picks.replace(']', '')
+        picks = picks.replace("'", '')
+        config['param_picks_by_channel_indices'] = list(map(str, picks.split(', ')))
+
 
     # Comments messages
     if config['param_freqs_specific_or_start'] is not None and config['param_freqs_end'] is None:
